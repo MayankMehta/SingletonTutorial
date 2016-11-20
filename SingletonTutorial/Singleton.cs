46f746b8e26625e05552647409359a562716446b
Mayank Mehta <mayank.mehta@live.com>
2016-11-20 13:04:21 -0500

    Commit

---
+++ SingletonTutorial/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SingletonTutorial
+{
+    public class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+
+        private Singleton()
+        {
+            //Stuff that must happen only once
+            Console.WriteLine("In Singleton Constructor");
+        }
+
+        public static Singleton Instance { get { return instance; } }
+
+        public static void SayHi()
+        {
+            // This must be thread-safe
+            Console.WriteLine("Hi there!");
+        }
+
+        public void DoSomething()
+        {
+            // This must be thread-safe
+        }
+    }
+}
---
+++ SingletonTutorial/SingletonClient.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+
+namespace SingletonTutorial
+{
+    [TestFixture]
+    public class SingletonClient
+    {
+        [Test]
+        public void UseSingleton()
+        {
+            Singleton.SayHi();
+            Console.WriteLine("Start of test");
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+            Assert.AreSame(s1, s2);
+        }
+    }
+}
